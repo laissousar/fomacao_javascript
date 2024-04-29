69730427d6d5064e4c99c37e5cf376cb01416ef8
Lais Rocha <106330472+laissousar@users.noreply.github.com>
2024-04-29 18:28:10 -0300

    exercicio1

---
+++ 05- listas (arrays)/tabuada.js
@@ -0,0 +1,8 @@
+// 1) Crie um programa que dado um número imprima a sua tabuada.
+
+const numero = 12;
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`)
+}
